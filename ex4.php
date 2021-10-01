cea4fc72c189871a7b6c04b5512bcba0f3439d50
ADCairex <angelmartinezguardiola@gmail.com>
2021-10-01 18:58:03 +0200

    finish the ex4

--- ex4.php
+++ ex4.php
@@ -1,5 +1,6 @@
 <?php
+    $hello = 'Hola';
     $name = $_GET['name'];
-    echo '<h1>Hola ' . $name . '</h1>';
-    echo '<center><p style="color: red;">hola</p></center>';
+    echo '<h1>' . $hello . ' ' . $name . '</h1>';
+    echo '<center><p style="color: red;"><b>hola</b></p></center>';
 ?>  
