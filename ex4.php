6ec5c9c6c4e5ef1eadcafc7808a98f6e8c9e374f
ADCairex <angelmartinezguardiola@gmail.com>
2021-10-01 18:55:41 +0200

    modify the ex4

--- ex4.php
+++ ex4.php
@@ -1,5 +1,5 @@
 <?php
     $name = $_GET['name'];
     echo '<h1>Hola ' . $name . '</h1>';
-    echo '<p style="color: red;">hola</p>';
+    echo '<center><p style="color: red;">hola</p></center>';
 ?>  
