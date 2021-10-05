68e894c3aca3c0e5ff126e51a54c6795df49b96b
ADCairex <angelmartinezguardiola@gmail.com>
2021-10-05 16:13:07 +0200

    finish ex17

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
     $data = file_get_contents('definir_json.json');
     $data = json_decode($data, true);
+
     $actual_date = getdate();
 
     $juniors = array();
@@ -13,7 +14,7 @@
         $year = $date[0];
         $month = $date[1];
         $day = $date[2];
-        $edad =$actual_date['year'] -  $year;
+        $edad = $actual_date['year'] -  $year;
         if ($month > $actual_date['mon'] || ($month = $actual_date['mon'] && $day > $actual_date['mday'])) {
             $edad = $edad - 1;
         }
@@ -21,7 +22,6 @@
     }
 
     foreach ($data as $i) {
-        echo $i['nombre'] . ': ' . calculateAge($i['fecha_nacimiento']). '<br>';
         if (calculateAge(($i['fecha_nacimiento'])) < 20) {
             array_push($juniors, $i);
         } elseif (calculateAge(($i['fecha_nacimiento'])) >= 20 && calculateAge(($i['fecha_nacimiento'])) < 40) {
@@ -31,8 +31,57 @@
         }
     }
 
-    foreach ($juniors as $value) {
-        echo $value['nombre'] . '<br>';
-    }
+    echo '<table>';
+        echo '<tr>';
+            echo '<th colspan="2">';
+                echo 'Juniors';
+            echo '<th>';
+        echo '</tr>';
+        foreach ($juniors as $value) {
+            echo '<tr>';
+                echo '<td>';
+                    echo $value['nombre'];
+                echo '</td>';
+                echo '<td>'; 
+                    echo calculateAge(($value['fecha_nacimiento']));
+                echo '</td>';
+            echo '</tr>';
+        }
+    echo '</table';
 
+    echo '<table>';
+        echo '<tr>';
+            echo '<th colspan="2">';
+                echo 'Masters';
+            echo '<th>';
+        echo '</tr>';
+        foreach ($masters as $value) {
+            echo '<tr>';
+                echo '<td>';
+                    echo $value['nombre'];
+                echo '</td>';
+                echo '<td>'; 
+                    echo calculateAge(($value['fecha_nacimiento']));
+                echo '</td>';
+            echo '</tr>';
+        }
+    echo '</table';
+
+    echo '<table>';
+        echo '<tr>';
+            echo '<th colspan="2">';
+                echo 'Seniors';
+            echo '<th>';
+        echo '</tr>';
+        foreach ($seniors as $value) {
+            echo '<tr>';
+                echo '<td>';
+                    echo $value['nombre'];
+                echo '</td>';
+                echo '<td>'; 
+                    echo calculateAge(($value['fecha_nacimiento']));
+                echo '</td>';
+            echo '</tr>';
+        }
+    echo '</table';
 ?>
